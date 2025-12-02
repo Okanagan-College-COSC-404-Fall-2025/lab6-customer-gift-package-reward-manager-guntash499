6d057b17c3710d1c8355909e86f7ef768f51a5ea
guntash499 <guntash.brarltd@gmail.com>
2025-12-02 10:54:47 -0800

    Create InsertValues.sql
    part-3

---
+++ Scripts/InsertValues.sql
@@ -0,0 +1,12 @@
+INSERT INTO gift_catalog
+VALUES (1, 100, gift_list_t('Stickers', 'Pen Set'));
+COMMIT;
+
+INSERT INTO gift_catalog
+VALUES (2, 1000, gift_list_t('Teddy Bear', 'Mug', 'Perfume Sample'));
+COMMIT;
+
+INSERT INTO gift_catalog
+VALUES (3, 10000, gift_list_t('Backpack', 'Thermos Bottle', 'Chocolate Collection'));
+
+COMMIT;
